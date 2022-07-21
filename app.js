b81e0d21e423f55da87648570b6c1745ec2b7024
Miguel Morillo <morillo_mig93@hotmail.com>
2022-07-21 19:16:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-    console.log(`App listen in port ${port}`)
+app.listen(3000 || process.env.PORT, () => {
+    console.log(`App listen in port ${port} process.env.PORT`)
 })
 
 //for telegram bot api
